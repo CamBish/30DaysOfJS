d51846fa95a5255eb7365fed5f030a08ffe2ce51
Cameron Bishop <17ctb3@queensu.ca>
2022-08-03 19:26:15 -0400

    day 1

---
+++ Day 1/introduction.js
@@ -0,0 +1,2 @@
+//welcome to 30 days of javascript!
+console.log('Welcome to 30DaysOfJavaScript')
---
+++ Day 1/variables.js
@@ -0,0 +1,26 @@
+
+//comments make code easier to read
+
+let firstName = "Cameron";
+let lastName = "Bishop";
+let country = "Canada";
+let city = "Kingston";
+let age = 22;
+let isMarried = false;
+
+myAge = age
+yourAge = 35
+
+console.log('my age is ' + myAge)
+console.log('your age is ' + yourAge)
+
+console.log(firstName, lastName, country, city, age, isMarried);
+
+const g = 9.81;
+const bp = 100;
+const PI = 3.14;
+
+console.log(g, bp, PI);
+
+let name = "Bob", job = "Web Developer", live = "New York";
+console.log(name, job, live);
